2450afc22439d513086741f88c0697c171edb456
Aziz Kemal HOSCAN <kemal.hoscan@yahoo.com>
2019-11-18 17:21:43 +0000

    Finalised roots for boss

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pokemons
   # Watch out for several or nested routes
 
-  # resources :users
+  resources :users
 end
