e4ba8d5cb358f86688da5f6566773142793a3f8f
Nicolas De Mil <nicodemil@hotmail.com>
2019-11-19 15:22:30 +0000

    Building routes and associations

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,29 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    user_is_owner?
+  end
+
+  def destroy?
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    @record.username == @user.username
+  end
+end
