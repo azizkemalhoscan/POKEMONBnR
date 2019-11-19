581261f3d850a8b0a89fe13598097520e7526eef
Susanna Jacob <susanna@mgenlearn.com>
2019-11-19 06:18:35 +0000

    Almost complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
   devise_for :views
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pokemons
   # Watch out for several or nested routes
 
-  #resources :users
+  resources :users
 end
