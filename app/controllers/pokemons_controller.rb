8ef877a2b970db79f345cdea69193c661a1a2b1b
Nicolas De Mil <nicodemil@hotmail.com>
2019-11-22 15:52:22 +0000

    heroku

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,9 +1,10 @@
 class PokemonsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     if params[:query].present?
       @pokemons = policy_scope(Pokemon).global_search(params[:query])
     else
-      @pokemons = policy_scope(Pokemon).paginate(page: params[:page]).per_page(10)
+      @pokemons = policy_scope(Pokemon).paginate(page: params[:page]).per_page(20)
     end
     authorize @pokemons
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_212258) do
+ActiveRecord::Schema.define(version: 2019_11_22_122926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 2019_11_21_212258) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "avatar"
+    t.integer "battles_fought", default: 0
+    t.integer "battles_won", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
